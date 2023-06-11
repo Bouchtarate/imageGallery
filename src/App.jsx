e42d1e9639c3449ec0ba8f4cac900e9956a34a49
Bouchtarate <hamza.bouchtarate@gmail.com>
2023-06-11 19:19:59 +0100

    complete the image gallery app using react

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,40 @@
+import React from "react";
+import { useState, useEffect } from "react";
 import "./style/tailwind.css";
+import ImageCard from "./components/ImageCard";
+import ImageSearch from "./components/ImageSearch";
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${
+        import.meta.env.VITE_PIXABAY_API_KEY
+      }&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [term]);
+  return (
+    <div className="container mx-auto">
+      <ImageSearch searchText={(text) => setTerm(text)} />
+      {!isLoading && images.length === 0 && (
+        <h1 className="text-6xl text-center mx-auto mt-32">
+          No Image in this keyword
+        </h1>
+      )}
+      <div className="grid grid-cols-3 gap-4">
+        {images.map((image) => (
+          <ImageCard key={image.id} image={image} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
